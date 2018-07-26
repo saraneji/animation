d0d43a28df99106ee463cb8da4b7c6785bd834f6
neji sara <nejisara2@gmail.com>
2018-07-26 13:57:23 +0200

    animations

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>hello</title>
+	<link rel="stylesheet" type="text/css" href="animation.css">
+</head>
+<body>
+	
+		
+			<img src="arrow.png" class="swing">
+	
+</body>
+</html>
